9d691749a954870f16cd793a7cb827b5fdf07747
DmytroChekun <snashd4@gmail.com>
2020-04-14 15:01:07 +0300

    checkpoint

--- js/practice.js
+++ js/practice.js
@@ -1,15 +1,26 @@
+let userText;
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
-        let userText = $('#searchText').val();
-        films(userText)
+        userText = $('#searchText').val();
+        films(userText);
         e.preventDefault();
+        loadMore(userText);
     });
   });
 
+let pages;
+let totalResults;
 let output = '';
 let convertData = function(respData) {
+    output = '';
+    totalResults = respData.totalResults;
+    pages = (respData.totalResults/10).toFixed(0);
+    if (respData.totalResults < 10){
+        pages = 0;
+    };
     for (let i = 0; i < respData.Search.length; i++) {
-        // console.log(respData.Search[i]);
+        console.log(respData.Search[i]);
+
         let movies = respData.Search[i];
         let movieName = movies.Title;
         let moviePoster = movies.Poster;
@@ -21,28 +32,40 @@ let convertData = function(respData) {
             </div>
             <p class="movie-card__title">${movieName}</p>
             <p class="movie-card__year">Year: ${year}</p>
-            <a onclick="movieSelected('${movies.imdbID}')" class="movie-card__btn btn" href="#">Movie Details</a>
+            <a onclick="getMovie('${movies.imdbID}')" class="movie-card__btn btn" href="#">Movie Details</a>
           </div>
       `;
      };
      
 }
-
+let selectorVal;
+$('input').change(function(){
+    if ($("#mov").is(':checked')==true){
+        selectorVal = "movie";
+    } else if($("#ser").is(':checked')==true){
+        selectorVal = "series";
+    } else if($("#ep").is(':checked')==true){
+        selectorVal = "episode";
+    }else{
+        selectorVal = null;
+    }
+});
 
 function films(userText) {
-    const apiUrl = `http://www.omdbapi.com/?s=${userText}&apikey=a506ace3`;
-
+    const apiUrl = `http://www.omdbapi.com/?s=${userText}&type=${selectorVal}&apikey=a506ace3`;
+    
     fetch(apiUrl)
         .then((response) => {
             return response.json();
         })
         .then((respData) => {
             convertData(respData);
-            // console.log(respData);
+            console.log(respData);
             $('#movies-list').html(output);
         })
         .catch(function(error) {
-            console.log("error!!");
+            console.log(error);
+            console.log("error from 54");
             swal({
                 title: "(ಠ_ಠ)",
                 text: "I can't find it. So... it doesn't exist. \n And yeah, I'm sure.",
@@ -53,59 +76,45 @@ function films(userText) {
 
 };
 
-function movieSelected(id){
-    sessionStorage.setItem('movieId', id);
-}
-
-
-
-let outputFilm = '';
-let mov;
+let outputFilm;
 let showMovie = function(respMov){
-    mov = response.data;
+    console.log(respMov.Poster);
     outputFilm =`
     <div class="modal">
-    <div class="modal-content">
-        <div class="row">
-            <div class="col-md-4">
-                <img src="${mov.Poster}" class="thumbnail">
+    <div class="modal-content container">
+        <div class="movie-info row">
+            <div class="movie-info__img-box col-lg-4 col-md-12">
+                <img class="movie-info__img"src="${respMov.Poster}">
             </div>
-            <div class="col-md-8">
-                <h2>${mov.Title}</h2>
+            <div class="movie-info__info col-lg-8 col-md-12">
+                <h2>${respMov.Title}</h2>
                 <ul class="list-group">
-                <li class="list-group-item"><strong>Genre:</strong> ${mov.Genre}</li>
-                <li class="list-group-item"><strong>Released:</strong> ${mov.Released}</li>
-                <li class="list-group-item"><strong>Rated:</strong> ${mov.Rated}</li>
-                <li class="list-group-item"><strong>IMDB Rating:</strong> ${mov.imdbRating}</li>
-                <li class="list-group-item"><strong>Director:</strong> ${mov.Director}</li>
-                <li class="list-group-item"><strong>Writer:</strong> ${mov.Writer}</li>
-                <li class="list-group-item"><strong>Actors:</strong> ${mov.Actors}</li>
+                    <li class="list-group-item"><strong>Genre:</strong> ${respMov.Genre}</li>
+                    <li class="list-group-item"><strong>Released:</strong> ${respMov.Released}</li>
+                    <li class="list-group-item"><strong>Rated:</strong> ${respMov.Rated}</li>
+                    <li class="list-group-item"><strong>IMDB Rating:</strong> ${respMov.imdbRating}</li>
+                    <li class="list-group-item"><strong>Director:</strong> ${respMov.Director}</li>
+                    <li class="list-group-item"><strong>Writer:</strong> ${respMov.Writer}</li>
+                    <li class="list-group-item"><strong>Actors:</strong> ${respMov.Actors}</li>
                 </ul>
             </div>
         </div>
-        <div class="row">
-            <div class="well">
-                <h3>Plot</h3>
-                ${mov.Plot}
+        <div class="row plot-sec">
+            <div class="offset-1 col-10">
+                ${respMov.Plot}
                 <hr>
-                <a href="http://imdb.com/title/${mov.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                <a href="http://imdb.com/title/${respMov.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
             </div>
         </div>
         <span class="close-btn">&times;</span>
     </div>
     </div>
-    `;
-}
-
-
-
-
-let movieId = sessionStorage.getItem('movieId');
-console.log(movieId);
-function getMovie(movieId){
-    const movInfo = `http://www.omdbapi.com/?i=${movieId}&apikey=a506ace3`;
-
+    `
+    ;
+};
+function getMovie(id){
+    const movInfo = `http://www.omdbapi.com/?i=${id}&apikey=a506ace3`;
+    console.log(movInfo);
     fetch(movInfo)
         .then((response) => {
             return response.json();
@@ -113,10 +122,20 @@ function getMovie(movieId){
         .then((respMov) => {
             console.log(respMov);
             showMovie(respMov);
-            $('#movieInfo').html(output);
+            $('#movieInfo').html(outputFilm);
+            $('.close-btn').on('click', function(){
+                $('.modal').addClass('hide');
+            });
+            $('.modal').on('click', function(event){
+                if (event.target == $(".modal")){
+                $('.modal').addClass('hide');
+            }
+            });
+            
         })
         .catch(function(error) {
-            console.log("error!!");
+            console.log(error);
+            console.log("error from 123");
             swal({
                 title: "(ಠ_ಠ)",
                 text: "I can't find it. So... it doesn't exist. \n And yeah, I'm sure.",
@@ -124,53 +143,62 @@ function getMovie(movieId){
                 dangerMode: true,
               });
         })
- 
     };
-
-let modal = $('.modal');
-let modalBtn = $('.movie-card__btn');
-let closeBtn = $('.close-btn');
-modalBtn.on('click', function(){
-    modal.style.display = "block";
-});
-closeBtn.on('click', function(){
-    modal.style.display = "none";
-});
-$(window).on('click', function(e){
-    if(e.target == modal){
-    modal.style.display = "none";
-}
-})
-
-
-
-
-// }
-// let switcherCheck;
-// $('input').change(function(){
-// if ($('#mov').is(':checked')==true){
-//     alert("penis");
-//     $(".movie-btn").css("background-color", "rgb(253, 13, 53)").css("width", "120px");
-// }else if ($('#mov').is(':checked')==false){
-//     $(".movie-btn").removeProp.css("background-color", "rgb(253, 13, 53)").css("width", "120px");
-// }
-// if($('#ser').is(':checked')){
-//     $(".series-btn").css("background-color", "rgb(253, 13, 53)").css("width", "120px");
-// }
-// else if($('#ep').is(':checked')){
-//     $(".episode-btn").css("background-color", "rgb(253, 13, 53)").css("width", "120px");
-// }
-// // else if($('input').not(':checked'))
-// });
-//                                                  NEEDED ATTR
-// Poster link img
-// Title
-// Type (movie)
-//year
-// imdbID: "tt0088247"
-// let convertData = function(respData) {
-//     for (let i = 0; i < respData.Search.length; i++) {
-//         console.log(respData.Search[i]);
-//      };
     
-// }
+
+
+$(window).scroll(function(){
+    if(totalResults>10&&$(window).scrollTop() == $(document).height() - $(window).height()){
+            loadMore();
+        }
+
+});
+let counter = 2;
+
+
+let loadMore = function() {
+        let apiUrl = `http://www.omdbapi.com/?s=${userText}&type=${selectorVal}&page=${counter}&apikey=a506ace3`;
+        console.log(apiUrl);
+        fetch(apiUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((respData) => {
+            convertDataNew(respData);
+            console.log(respData);
+            $('#movies-list').html(output);
+        })
+        .catch(function(error) {
+            console.log(error);
+            swal({
+                title: "(ಠ_ಠ)",
+                text: "I can't find it. So... it doesn't exist. \n And yeah, I'm sure.",
+                icon: "warning",
+                dangerMode: true,
+              });
+        })
+        let convertDataNew = function(respData) {
+            for (let i = 0; i < respData.Search.length; i++) {
+                let movies = respData.Search[i];
+                let movieName = movies.Title;
+                let moviePoster = movies.Poster;
+                let year = movies.Year;
+                pages = (respData.totalResults/10).toFixed(0);
+                
+                output += `
+                  <div class="movie-card">
+                    <div class="movie-card__poster-wrap">
+                        <img class="movie-card__poster" src="${moviePoster}" alt="image is not available">
+                    </div>
+                    <p class="movie-card__title">${movieName}</p>
+                    <p class="movie-card__year">Year: ${year}</p>
+                    <a onclick="getMovie('${movies.imdbID}')" class="movie-card__btn btn" href="#">Movie Details</a>
+                  </div>
+              `;
+             };
+            
+        if(loadMore){
+            counter += 1;
+        }
+};
+};
